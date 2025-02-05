ff6752138734d81769a6cdc8852d00a64a1c37c8
Miska <miska.nurminen@gmail.com>
2025-02-05 12:53:45 +0200

    jotain tehty

--- vk4/src/js/main.js
+++ vk4/src/js/main.js
@@ -1,8 +1,15 @@
 import '../css/style.css';
 import '../css/snackbar.css';
 import { getItems } from './items.js';
+import { getUsers, addUser } from './users.js';
 
 document.querySelector('#app').innerHTML = 'Moi tässä oman APIn harjoituksia';
 
 const getItemButton = document.querySelector('.get_items');
 getItemButton.addEventListener('click', getItems);
+
+const getUsersButton = document.querySelector('.get_users');
+getUsersButton.addEventListener('click', getUsers);
+
+const addUserForm = document.querySelector('.formpost');
+addUserForm.addEventListener('click', addUser);
---
+++ vk4/src/js/users.js
@@ -0,0 +1,57 @@
+import { fetchData } from './fetch';
+
+const getUsers = async () => {
+	const url = 'http://localhost:3000/api/users';
+	const users = await fetchData(url);
+
+	if (users.error) {
+		console.error('Virhe', users.error);
+		return;
+	}
+
+	console.log(users);
+
+	const tableBody = document.querySelector('.tbody');
+	tableBody.innerHTML = '';
+
+	users.forEach((user) => {
+		const row = document.createElement('tr');
+		row.innerHTML = `
+            <td>${user.username}</td>
+            <td>${user.email}</td>
+            <td><button class="check" data-id="${user.id}">Info</button></td>
+            <td><button class="delete" data-id="${user.id}">Delete</button></td>
+            <td>${user.id}</td>
+        `;
+		tableBody.appendChild(row);
+	});
+};
+
+const addUser = async () => {
+	event.preventDefault();
+
+	const url = 'http://localhost:3000/api/users';
+	const username = document.querySelector('#username').value.trim();
+	const password = document.querySelector('#password').value.trim();
+	const email = document.querySelector('#email').value.trim();
+
+	const bodyData = {
+		username: username,
+		password: password,
+		email: email,
+	};
+
+	const options = {
+		body: JSON.stringify(bodyData),
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+		},
+	};
+	console.log(options);
+
+	const response = await fetchData(url, options);
+	console.log(response);
+};
+
+export { getUsers, addUser };
